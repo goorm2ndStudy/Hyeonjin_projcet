861b67a8e9ab588c95b099a6d99c07666df8a387
ko <iamkokoko123@gmail.com>
2023-12-05 15:43:40 +0900

    start study

---
+++ wild/src/test/java/study/wild/WildApplicationTests.java
@@ -0,0 +1,13 @@
+package study.wild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
