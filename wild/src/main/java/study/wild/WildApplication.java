96ac1560bfa51f5a483a3be8823fe1f5abf70a8d
ko <iamkokoko123@gmail.com>
2023-12-06 16:48:56 +0900

    setting

---
+++ wild/src/main/java/study/wild/WildApplication.java
@@ -0,0 +1,14 @@
+package study.wild;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class WildApplication {
+
+
+    void contextLoads() {
+
+    }
+
+}
